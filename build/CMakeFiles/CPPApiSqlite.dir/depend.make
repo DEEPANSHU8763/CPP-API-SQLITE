bc2d0cea37711d9ae89217546e957457e169c409
DEEPANSHU <93826687+DEEPANSHU8763@users.noreply.github.com>
2025-06-21 23:17:29 +0530

    Add files via upload

---
+++ build/CMakeFiles/CPPApiSqlite.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPPApiSqlite.
+# This may be replaced when dependencies are built.
---
+++ database.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+bool initializeDatabase();
+bool insertUsers(const std::string& jsonData);
